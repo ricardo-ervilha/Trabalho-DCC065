153c5ef2c0f0e17a1ebb2b68e6a9daa1af36b361
Lucas Santana <lucasg4x@gmail.com>
2023-05-14 01:40:20 -0300

    Load plane in Airplane class and move plane using RayCaster

--- CG-gh-pages/T2/airplane.js
+++ CG-gh-pages/T2/airplane.js
@@ -1,52 +1,63 @@
-import * as THREE from  'three';
-import {GLTFLoader} from '../build/jsm/loaders/GLTFLoader.js';
+import * as THREE from "three";
+import { GLTFLoader } from "../build/jsm/loaders/GLTFLoader.js";
+import { scale} from './variables.js';
+import {getMaxSize} from "../libs/util/util.js";
+import { AxesHelper, MathUtils } from '../build/three.module.js';
+export class Airplane {
+  constructor() {
+    this.airplane = null;
+  }
 
-export class Airplane{
+  getAirplane(){
+    return this.airplane;
+  }
 
-    constructor(){
-        
-    }
 
-    //Função que cria, instancia e retorna o avião.
-    async buildAirPlane() {
-        //Instancio o loader
-        var loader = new GLTFLoader();
-        let obj;
-        //Carrego o arquivo glb nele
-        await loader.load('./airplane.glb', (gltf) => {
-            console.log(obj);
-            obj = gltf.scene;
-            obj.visible = true;
-            obj.traverse(function (child) {
-                if (child) {
-                    child.castShadow = true;
-                }
-            });
-            obj.traverse(function (node) {
-                if (node.material) node.material.side = THREE.DoubleSide;
-            });
-
+  buildAirPlane(scene) {
+    var loader = new GLTFLoader( );
+    loader.load( 'airplane.glb', ( gltf ) => {
+        var obj = gltf.scene;
+        obj.traverse( function ( child ) {
+        if ( child ) {
+            child.castShadow = true;
+        }
         });
-        console.log(obj);
-        
-        return obj;
-    }
+        obj.traverse( function( node )
+        {
+        if( node.material ) node.material.side = THREE.DoubleSide;
+        });
 
-    normalizeAndRescale(obj, newScale) {
-        var scale = getMaxSize(obj);
-        obj.scale.set(newScale * (1.0 / scale),
-            newScale * (1.0 / scale),
-            newScale * (1.0 / scale));
-        return obj;
-    }
+        obj = this.normalizeAndRescale(obj, scale);
+        //obj = this.fixPosition(obj);
+        obj.position.y = 10;
+        obj.rotateY(MathUtils.degToRad(-90));
+        this.airplane = obj;
+        this.airplane.add(new THREE.AxesHelper( 12 ));
+        scene.add(obj);
 
-    // fixPosition(obj) {
-    //     // Fix position of the object over the ground plane
-    //     var box = new THREE.Box3().setFromObject(obj);
-    //     if (box.min.y > 0)
-    //         obj.translateY(-box.min.y);
-    //     else
-    //         obj.translateY(-1 * box.min.y);
-    //     return obj;
-    // }
+    },this.onProgress, this.onError);
+  }
+
+  onError() {}
+
+  onProgress() {}
+
+  normalizeAndRescale(obj, newScale) {
+    //Normaliza o objeto e multiplica por uma nova escala
+    var scale = getMaxSize(obj);
+    obj.scale.set(
+      newScale * (1.0 / scale),
+      newScale * (1.0 / scale),
+      newScale * (1.0 / scale)
+    );
+    return obj;
+  }
+
+  fixPosition(obj) {
+    // Corrige a posição do objeto acima do plano
+    var box = new THREE.Box3().setFromObject(obj);
+    if (box.min.y > 0) obj.translateY(-box.min.y);
+    else obj.translateY(-1 * box.min.y);
+    return obj;
+  }
 }
--- CG-gh-pages/T2/basic.js
+++ CG-gh-pages/T2/basic.js
@@ -7,27 +7,51 @@ import {initRenderer,
         InfoBox,
         onWindowResize,
         createGroundPlaneXZ,
-    getMaxSize} from "../libs/util/util.js";
+        getMaxSize} from "../libs/util/util.js";
 import {GLTFLoader} from '../build/jsm/loaders/GLTFLoader.js';
 import { airPlaneHeight, numPlans, scale} from './variables.js';
 import { Environment } from './environment.js';
 import { MathUtils } from '../build/three.module.js';
 import {Queue} from './queue.js';
+import { Airplane } from "./airplane.js";
 
 let scene, renderer, camera, material, light, orbit; // Initial variables
+let pointer = new THREE.Vector2();// posição do mouse na tela
+const lerpConfig = { destination: new THREE.Vector3(0.0, 0.0, 0.0), alpha: 0.05 }//posição destino para a qual o avião vai se deslocar
+
 scene = new THREE.Scene();    // Create main scene
 renderer = initRenderer();    // Init a basic renderer
-camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
+camera = initCamera(new THREE.Vector3(0, 30, 100)); // Init camera in this position
 material = setDefaultMaterial(); // create a basic material
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
 orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
 
 // Listen window size changes
 window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+window.addEventListener('mousemove', onMouseMove);//quando o mouse mover, atualiza a posição destino
+
+//Cria o avião e o adiciona na cena
+let aviao = new Airplane();
+aviao.buildAirPlane(scene);
+
+// -- Cria o raycaster que será usado para fazer interseção entre o plano e a posição do mouse
+let raycaster = new THREE.Raycaster();
+
+// Cria o plano que será usado para fazer interseção com o mouse
+let plane, planeGeometry, planeMaterial;
+
+planeGeometry = new THREE.PlaneGeometry(150, 30, 1, 1);
+planeMaterial = new THREE.MeshLambertMaterial();
+planeMaterial.side = THREE.DoubleSide;
+planeMaterial.transparent = true;
+planeMaterial.opacity = 1;
+plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.translateY(15);//move para cima para evitar que o avião passe abaixo do plano
+scene.add(plane);
 
 // Show axes (parameter is size of each axis)
 let axesHelper = new THREE.AxesHelper( 12 );
-scene.add( axesHelper );
+scene.add(axesHelper);
 
 var queue = new Queue();
 // O oitavo plano será o que estará na parte de reposição.
@@ -45,70 +69,6 @@ for(var i = 0; i < numPlans; i++){
     scene.add(plane);
 
     queue.enqueue(environment);
-}
-
-//Cria o avião
-var airplane = null;
-
-// Load animated files
-loadGLTFFile('airplane.glb');
-
-render();
-
-
-function loadGLTFFile(modelName)
-{
-  var loader = new GLTFLoader( );
-  loader.load( modelName, function ( gltf ) {
-    var obj = gltf.scene;
-    obj.traverse( function ( child ) {
-      if ( child ) {
-          child.castShadow = true;
-      }
-    });
-    obj.traverse( function( node )
-    {
-      if( node.material ) node.material.side = THREE.DoubleSide;
-    });
-
-      obj = normalizeAndRescale(obj, scale);
-      obj = fixPosition(obj);
-      obj.position.y = 10;
-      obj.rotateY(MathUtils.degToRad(-90));
-      airplane = obj;
-
-    
-    scene.add ( obj );
-
-    }, onProgress, onError);
-}
-
-function onError() { };
-
-function onProgress () {
-    
-}
-
-
-function normalizeAndRescale(obj, newScale) {
-
-    //Normaliza o objeto e multiplica por uma nova escala
-    var scale = getMaxSize(obj);
-    obj.scale.set(newScale * (1.0 / scale),
-        newScale * (1.0 / scale),
-        newScale * (1.0 / scale));
-    return obj;
-}
-
-function fixPosition(obj) {
-
-    // Corrige a posição do objeto acima do plano
-    var box = new THREE.Box3().setFromObject(obj);
-    if (box.min.y > 0)
-        obj.translateY(-box.min.y);
-    else
-        obj.translateY(-1 * box.min.y);
-    return obj;
 }
 
 /*
@@ -135,23 +95,76 @@ function updatePositionPlanes(){
             plane.material.opacity = 1;
         }
     }
-    console.log(count);
+    //console.log(count);
+}
+
+/**
+ * Função chamada quando o evento mover o mouse é disparado
+ * @param {*} event 
+ */
+
+function onMouseMove(event){
+    if(aviao.getAirplane()){
+        // calculate pointer position in normalized device coordinates
+        // (-1 to +1) for both components
+        pointer.x =  (event.clientX / window.innerWidth) * 2 - 1;
+        pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        // update the picking ray with the camera and pointer position
+        raycaster.setFromCamera(pointer, camera);
+
+        // calculate plane intersecting the picking ray
+        let intersects = raycaster.intersectObject(plane);
+
+        if (intersects.length > 0) // Check if there is a intersection
+        {
+            let point = intersects[0].point; // Pick the point where interception occurrs
+
+            if(plane == intersects[0].object ) {
+                lerpConfig.destination.x = point.x;
+                lerpConfig.destination.y = point.y;
+            }
+        }
+    }
+};
+
+/**
+ * Função para fazer a rotação do avião
+ */
+function rotateAirplane(){
+    if(aviao.getAirplane()){
+        let rad = THREE.MathUtils.degToRad(45);
+        let quat = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), rad);
+        //aviao.getAirplane().quaternion.slerp(quat, 0.01);
+    }
+}
+
+/**
+ * Função para mover o avião para  a posição do mouse
+ */
+function moveAirPlane(){
+    if(aviao.getAirplane()){
+        aviao.getAirplane().position.lerp(lerpConfig.destination, lerpConfig.alpha);
+        rotateAirplane();
+    }
 }
 
 // Use this to show information onscreen
 let controls = new InfoBox();
-  controls.add("Basic Scene");
-  controls.addParagraph();
-  controls.add("Use mouse to interact:");
-  controls.add("* Left button to rotate");
-  controls.add("* Right button to translate (pan)");
-  controls.add("* Scroll to zoom in/out.");
-  controls.show();
+controls.add("Basic Scene");
+controls.addParagraph();
+controls.add("Use mouse to interact:");
+controls.add("* Left button to rotate");
+controls.add("* Right button to translate (pan)");
+controls.add("* Scroll to zoom in/out.");
+controls.show();
 
+render();
 function render()
-{
-  //Para usar o airplane, botar um if dentro desse render.
-  updatePositionPlanes();
-  requestAnimationFrame(render);
-  renderer.render(scene, camera) // Render scene
+{ 
+    moveAirPlane();
+    updatePositionPlanes();
+
+    requestAnimationFrame(render);
+    renderer.render(scene, camera) // Render scene
 }
